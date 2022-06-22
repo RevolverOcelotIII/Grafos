052a793a935a046c15c669fc05f496aebcf190b6
Revolver Ocelot(Revolver Ocelot) <47671687+RevolverOcelotIII@users.noreply.github.com>
2022-06-22 12:12:24 -0300

    Grafos update

--- Grafo.java
+++ Grafo.java
@@ -1,6 +1,8 @@
-package com.mycompany.grafos1;
+package com.mycompany.grafos;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +12,10 @@ public class Grafo {
     private List<Vertice> vertices;
     private List<Aresta> arestas;
     
-    
+    public Grafo(){
+        vertices = new ArrayList<>();
+        arestas = new ArrayList<>();
+    }
     private String nome;
 
     void adicionarVertice(int valor){
@@ -19,10 +24,9 @@ public class Grafo {
     
     void removerVertice (int valor){
         vertices.remove(new Vertice(valor));
-        arestas.stream().filter(a -> (a.getVertice1().getValor()==valor||a
-                .getVertice2().getValor()==valor)).forEachOrdered(a -> {
-            arestas.remove(a);
-        });
+        arestas.stream().filter(a -> a.getVertice1().getValor()==valor||a
+                .getVertice2().getValor()==valor)
+                .forEach(a -> arestas.remove(a));
     }
     boolean procurarVertice (int valor){
         return vertices.contains(new Vertice(valor));
@@ -39,12 +43,56 @@ public class Grafo {
     void removerAresta (int valor_1, int valor_2){
         Vertice v1 = new Vertice(valor_1);
         Vertice v2 = new Vertice(valor_2);
-        List<Vertice> lista_vertice1 = adjVertices.get(v1);
-        List<Vertice> lista_vertice2 = adjVertices.get(v2);
-        if(lista_vertice1 != null) lista_vertice1.remove(v2);
-        if(lista_vertice2 != null) lista_vertice2.remove(v1);
+        arestas.stream().filter(a -> a.getVertice1()==v1&&a.getVertice2()==v2)
+                .forEach(a -> arestas.remove(a));
+    }
+    
+    boolean procurarAresta(int valor_1, int valor_2){
+        Vertice v1 = new Vertice(valor_1);
+        Vertice v2 = new Vertice(valor_2);
+        return arestas.contains(new Aresta(v1,v2));
     }
    
+    List<Vertice> verticesAdjacentes(int valor){
+        Vertice v = new Vertice(valor);
+        List<Vertice> adjacentes = new ArrayList<>();
+        arestas.stream().filter(a -> a.getVertice1()==v || a.getVertice2()==v)
+                .forEach(a -> {
+                    if(a.getVertice1()==v)adjacentes.add(a.getVertice2());
+                    else adjacentes.add(a.getVertice1());
+                });
+        return adjacentes;
+    }
+    int verticeGrau(int valor){
+        Vertice v = new Vertice(valor);
+        return (int) arestas.stream().filter(a -> a.getVertice1()==v || a.getVertice2()==v)
+                .count();
+    }
+    int menorGrau(){
+        List<Integer> graus = new ArrayList<>();
+        vertices.forEach(v -> {
+            graus.add(verticeGrau(v.getValor()));
+        });
+        return Collections.min(graus);
+    }
+    int maiorGrau(){
+        List<Integer> graus = new ArrayList<>();
+        vertices.forEach(v -> {
+            graus.add(verticeGrau(v.getValor()));
+        });
+        return Collections.max(graus);
+    }
+    int grauMedio(){
+        int grau_soma;
+        
+        grau_soma = vertices.stream()
+                .mapToInt(it -> verticeGrau(it.getValor()))
+                .sum();
+        return grau_soma/vertices.size();
+    }
+    boolean verificaConexo(int valor){
+        List<Vertice> vertices = 
+    }
     Grafo criarGrafo() {
         Grafo grafo = new Grafo();
         grafo.adicionarVertice(1);
